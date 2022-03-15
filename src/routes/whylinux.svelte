f7cf42cf5b0bc2394b1d85a859c421a8fd19dc25
Aveerator <aganitnt@gmail.com>
2022-03-15 20:08:45 +0100

    whylinux

---
+++ src/routes/whylinux.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { titleBar } from '../stores/store';
+	$titleBar = 'Todo List';
+</script>
+
+<div class="maincontainer-flex columns-1">
+    Dlaczego używam GNU/Linuxa?:
+     - Ładniej wygląda, Możliwość customizacji (Mnogość Desktopów i Ustawień)
+     - Łatwość w instalowaniu aplikacji (Narzędzia jak GNOME desktop, i cli tak jak apt, pacman, yay)
+     - Prędkość włączania (1-5 sekund)
+     - Możliwość przejrzenia kodu źródłowego swojego systemu (Nie ma w chuj bloatu i wirusów jak <b>INNE SYSTEMY</b>)
+     - Mniejsze zużycie zasobów komputera (ok. 10% zużycia windowsa)
+</div>
